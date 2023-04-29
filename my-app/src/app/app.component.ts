54b8dc4a2a8f939982deeaaeb2f2c324bb45ae2f
ejgdr <gui20006@byui.edu>
2023-04-29 09:08:54 -0600

    section 2 completed

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+
+  .white-text {
+    color: white;
+  }
+  `]
 })
 export class AppComponent {
+  username = '';
+  showSecret = false;
+  log : Array<any> = [];
 
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
---
+++ my-app/src/app/servers/servers.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created!';
+  serverName = 'testServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =  'Server was created! Name is ' + this.serverName; 
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
