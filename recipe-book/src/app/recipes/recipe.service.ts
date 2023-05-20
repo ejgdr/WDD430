27162cf4f75d1d70f2151f31215f699e8a8d8cfe
ejgdr <gui20006@byui.edu>
2023-05-20 16:23:07 -0600

    Section 10 completed

---
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) { }
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Pork Ribs',
+      'This is an awesome recipe',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Pork Ribs', 2),
+        new Ingredient('BBQ Sauce', 1)
+      ]),
+    new Recipe(
+      'Gluten Free Bread',
+      'This is simply the best bread ever',
+      'https://cdn.pixabay.com/photo/2016/06/16/01/41/bread-1460403_640.jpg',
+      [
+        new Ingredient('Whole grain flower', 1),
+        new Ingredient('Gluten free yeast', 1)
+      ])
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+}
