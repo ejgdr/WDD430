95e11f7145a55605d340171270b2080992fae134
ejgdr <gui20006@byui.edu>
2023-06-16 11:02:25 -0600

    Section 19 completed

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -13,24 +13,32 @@ export class RecipeService {
 
   constructor(private slService: ShoppingListService) { }
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Pork Ribs',
-      'This is an awesome recipe',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Pork Ribs', 2),
-        new Ingredient('BBQ Sauce', 1)
-      ]),
-    new Recipe(
-      'Gluten Free Bread',
-      'This is simply the best bread ever',
-      'https://cdn.pixabay.com/photo/2016/06/16/01/41/bread-1460403_640.jpg',
-      [
-        new Ingredient('Whole grain flower', 1),
-        new Ingredient('Gluten free yeast', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Pork Ribs',
+  //     'This is an awesome recipe',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [
+  //       new Ingredient('Pork Ribs', 2),
+  //       new Ingredient('BBQ Sauce', 1)
+  //     ]),
+  //   new Recipe(
+  //     'Gluten Free Bread',
+  //     'This is simply the best bread ever',
+  //     'https://cdn.pixabay.com/photo/2016/06/16/01/41/bread-1460403_640.jpg',
+  //     [
+  //       new Ingredient('Whole grain flower', 1),
+  //       new Ingredient('Gluten free yeast', 1)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
+
+  // made to override the list we have here and work from the database
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
