3e72e9759d3abfd3c7d143daf6335fb96e4700b3
ejgdr <gui20006@byui.edu>
2023-06-16 00:12:51 -0600

    Section 18 completed

---
+++ http-requests/src/app/auth-interceptor.service.ts
@@ -0,0 +1,17 @@
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    
+    const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
+    return next.handle(modifiedRequest);
+  }
+}
